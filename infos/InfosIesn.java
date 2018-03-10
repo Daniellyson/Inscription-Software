434f4a7ffd26d8f95f06637b5d729bb93a06a2ca
Daniellyson <etu32290@henallux.be>
2018-03-10 16:26:05 +0100

    Inscription Software

--- infos/InfosIesn.java
+++ infos/InfosIesn.java
@@ -9,8 +9,8 @@ public class InfosIesn extends JPanel {
     public InfosIesn() {
 
         text = new JLabel("<html> <br> <b>Contact details: </b> <br><br>" +
-                "Address : Street Neverland, 0 - 0000 Belgium<br>" +
-                "Tel : +32 (0)00 00 00 00<br>" +
+                "Address : Street Neverland, 0 - 0000 Belgium <br>" +
+                "Tel : +32 (0)00 00 00 00 <br>" +
                 "Web site : www.website.be</html>");
         text.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 
