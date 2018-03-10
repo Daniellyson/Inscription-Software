d409d8b32fec5d63d676e0cb1de17288b4166675
Daniellyson <etu32290@henallux.be>
2018-03-10 13:25:24 +0100

    Infos

--- infos/InfosIesn.java
+++ infos/InfosIesn.java
@@ -4,26 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class InfosIesn extends JPanel {
-    private  JLabel title;
     private JLabel text;
 
     public InfosIesn() {
 
-        title = new JLabel("Contact details");
-        title.setFont(new Font("Times New Roman", Font.BOLD, 25));
-
-        text = new JLabel("<html>Address : Street Neverland, 00 - 0000 Belgium<br>" +
+        text = new JLabel("<html> <br> <b>Contact details: </b> <br><br>" +
+                "Address : Street Neverland, 0 - 0000 Belgium<br>" +
                 "Tel : +32 (0)00 00 00 00<br>" +
-                "Web site : www.henallux.be</html>");
-        text.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+                "Web site : www.website.be</html>");
+        text.setFont(new Font("Times New Roman", Font.PLAIN, 23));
 
-        title.setHorizontalAlignment(SwingConstants.CENTER);
         text.setHorizontalAlignment(SwingConstants.CENTER);
 
         this.setLayout(new BorderLayout());
 
-        this.add(title, BorderLayout.NORTH);
-        this.add(text, BorderLayout.CENTER);
+        this.add(text, BorderLayout.NORTH);
 
         this.setVisible(true);
     }
