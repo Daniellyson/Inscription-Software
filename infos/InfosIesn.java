37bab6ea8507ae7d2a64496920ede0dfdd688804
Daniellyson <etu32290@henallux.be>
2018-03-11 08:58:31 +0100

    Info

--- infos/InfosIesn.java
+++ infos/InfosIesn.java
@@ -5,13 +5,16 @@ import java.awt.*;
 
 public class InfosIesn extends JPanel {
     private JLabel text;
+	
+	private JLabel webLabel;
 
     public InfosIesn() {
+        webLabel = new JLabel("http://www.website.com");
 
-        text = new JLabel("<html> <br> <b>Contact details: </b> <br><br>" +
-                "Address : Street Neverland, 0 - 0000 Belgium <br>" +
-                "Tel : +32 (0)00 00 00 00 <br>" +
-                "Web site : www.website.be</html>");
+        text = new JLabel("<html> <br> <b> Contact details </b> <br><br>" +
+                "Address : Street de la Pluie, 00 - 0000 Belgium<br>" +
+                "Tel : +32 (0)00 00 00 00<br>" +
+                "Web site : <a href=" + webLabel + "> www.site.com </a> </html>");
         text.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 
         text.setHorizontalAlignment(SwingConstants.CENTER);
