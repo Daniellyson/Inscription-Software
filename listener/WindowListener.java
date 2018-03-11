f2464c0f392ac1d2d97d58283d86d3bbee4a3578
Daniellyson <etu32290@henallux.be>
2018-03-11 08:47:10 +0100

    Listener

---
+++ listener/WindowListener.java
@@ -0,0 +1,11 @@
+package listener;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class WindowListener extends WindowAdapter {
+    public void windowClosing(WindowEvent e) {
+
+        System.exit(0);
+    }
+}
